239260f1a4c2b334054bde11030874924172e175
AnthonyMadia <aamadia24@gmail.com>
2023-03-23 16:28:15 -0400

    add: blockchain dir

--- blockchain/abi.js
+++ blockchain/abi.js
@@ -1,11 +1,9 @@
 const fs = require('fs')
 const path = require('path')
 
-const ABI = require('../build/artifacts/contracts/Contract.sol/Contract.json')
-ABI.bytecode = ''
-ABI.deployedBytecode = ''
+const { abi } = require('../build/artifacts/contracts/Contract.sol/Contract.json')
 
 fs.writeFileSync(
     path.join(__dirname, '../abi/Contract.json'),
-    JSON.stringify(ABI)
+    JSON.stringify(abi)
 )
---
+++ blockchain/readme.md
@@ -0,0 +1,5 @@
+### Useful Blockchain Scripts and Functions
+
+---
+
+Purely educational repo serving as a collection of functionality and scripts useful for blockchain things.
